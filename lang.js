bde19d87e1c53a91ddfd9c60220483be4fb019e3
eddylily <edcreativity@gmail.com>
2024-09-16 23:26:03 +0900

    240916 alpha2

--- lang.js
+++ lang.js
@@ -1,46 +1,51 @@
-var langs = {
-    ko: {
-        hello: '나비처럼 날아올라',
-        myname: '✱Ed (에드)'
-    },
-    jp: {
-        hello: '飛び立つ。蝶々のように。',
-        myname: '✱Ed (エド)'
-    },
-    en: {
-        hello: 'Fly Up. Just Like a Butterfly.',
-        myname: '✱Ed'
-    }
-};
-  
-if (getLanguage() === 'ko-KR' || getLanguage() === 'ko') {
-    var elem = $('[data-lang]');
-    for(var i = 0; i < elem.length ; i++) {
-        var textKey = elem.eq(i).attr('data-lang');
-        var text = langs.ko[textKey];
-        elem.eq(i).html(text);
-    }
-}
-
-if (getLanguage() === 'ja-JP' || getLanguage() === 'ja') {
-    var elem = $('[data-lang]');
-    for(var i = 0; i < elem.length ; i++) {
-        var textKey = elem.eq(i).attr('data-lang');
-        var text = langs.jp[textKey];
-        elem.eq(i).html(text);
-    }
-}
-
-if (getLanguage() === 'en-US' || getLanguage() === 'en') {
-    var elem = $('[data-lang]');
-    for(var i = 0; i < elem.length ; i++) {
-        var textKey = elem.eq(i).attr('data-lang');
-        var text = langs.en[textKey];
-        elem.eq(i).html(text);
-    }
-}
-  
 function getLanguage() {
-    console.log(getLanguage());
     return navigator.language;
 }
+
+$(document).ready(function() {
+    var langs = {
+        ko: {
+            hello: '나비처럼 날아올라',
+            myname: '✱Ed (에드)'
+        },
+        jp: {
+            hello: '飛び立つ。蝶々のように。',
+            myname: '✱Ed (エド)'
+        },
+        en: {
+            hello: 'Fly Up. Just Like a Butterfly.',
+            myname: '✱Ed'
+        }
+    };
+
+    var currentLang = getLanguage();
+    
+    if (currentLang === 'ko-KR' || currentLang === 'ko') {
+        console.log("Deteceted ko-KR");
+        var elem = $('[data-lang]');
+        for(var i = 0; i < elem.length ; i++) {
+            var textKey = elem.eq(i).attr('data-lang');
+            var text = langs.ko[textKey];
+            elem.eq(i).html(text);
+        }
+    }
+
+    else if (currentLang === 'ja-JP' || currentLang === 'ja') {
+        console.log("Deteceted ja-JP");
+        var elem = $('[data-lang]');
+        for(var i = 0; i < elem.length ; i++) {
+            var textKey = elem.eq(i).attr('data-lang');
+            var text = langs.jp[textKey];
+            elem.eq(i).html(text);
+        }
+    }
+
+    else {
+        var elem = $('[data-lang]');
+        for(var i = 0; i < elem.length ; i++) {
+            var textKey = elem.eq(i).attr('data-lang');
+            var text = langs.en[textKey];
+            elem.eq(i).html(text);
+        }
+    }
+});
